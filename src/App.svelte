850f0f14bae9a7ccbe07b3ad92685ce0ba3bb034
Piia <piiapiia711@gmail.com>
2020-05-22 02:45:26 +0200

    initial commit

---
+++ src/App.svelte
@@ -0,0 +1,57 @@
+<script>
+	import Games from './Games.svelte';
+
+
+</script>
+
+<div class="container">
+<header>
+	<h1>Your Games List</h1>
+</header>
+</div>
+<div class="wrapper">
+	<Games />
+</div>
+
+<style>
+	.container {
+		display: flex;
+		justify-content: center;
+		flex: 1 1 0;
+		background-image: url("https://i.imgur.com/40mADzB.png");
+	}
+
+	header {
+		text-align: center;
+		padding: 1em;
+		width: 1000px;
+		display: flex;
+		justify-content: center;
+		color: #e8e6e6;
+	}
+
+	header h1 {
+		padding: 35px;
+		border-radius: 70px;
+		font-size: 50px;
+		background-color: #42474b;
+		transition: 0.3s;
+	}
+
+	@media only screen and (max-width: 640px) {
+		header h1 {
+			padding: 25px;
+			border-radius: 50px;
+			font-size: 35px;
+			background-color: #42474b;
+		}
+	}
+
+	.wrapper {
+		width: 100%;
+		padding: 40px 0;
+		background-color: rgb(241, 239, 239);
+	}
+
+
+</style>
---
+++ src/Games.svelte
@@ -0,0 +1,239 @@
+<script>
+import {games} from './data-games.js'
+import {onMount} from 'svelte'
+
+onMount( async() => {
+    const url = 'http://localhost/back/get-games.php'
+    let res = await fetch(url)
+    res = await res.json()
+    let data = await res.data
+    $games = res.data
+    console.log(res.data)
+})
+</script>
+
+<div class="flex-container">
+    {#each $games as game (game.id)}
+        <div class="cover {game.cover_dimensions}">
+            <a href={game.link}><img src={game.cover_image} alt=""></a>
+        </div>
+    {/each}
+</div>
+
+<style>
+
+.flex-container {
+    display: flex;
+    margin: 0 auto;
+    flex-flow: wrap;
+    max-width: 1000px;
+    justify-content: flex-start;
+}
+
+.flex-container img {
+    width: 100%;
+    padding: 5px;
+    border-radius: 10px;
+    box-sizing: border-box;
+}
+
+.flex-container img:hover {
+    background-color: rgba(43, 162, 199, 0.664);
+    cursor: pointer;
+}
+
+.flex-container .cover {
+    display: flex;
+    align-items: center;
+}
+
+.flex-container .nes {
+    flex: 4 4 125px;
+    max-width: 250px;
+    max-height: auto;
+    box-sizing: border-box;
+}
+
+.flex-container .ps2 {
+    flex: 4 4 125px;
+    max-width: 250px;
+    max-height: auto;
+    box-sizing: border-box;
+}
+
+.flex-container .xbox {
+    flex: 4 4 125px;
+    max-width: 250px;
+    max-height: auto;
+    box-sizing: border-box;
+}
+
+.flex-container .gcn {
+    flex: 4 4 125px;
+    max-width: 250px;
+    max-height: auto;
+    box-sizing: border-box;
+}
+
+.flex-container .xbox360 {
+    flex: 4 4 125px;
+    max-width: 250px;
+    max-height: auto;
+    box-sizing: border-box;
+}
+
+.flex-container .wii {
+    flex: 4 4 125px;
+    max-width: 250px;
+    max-height: auto;
+    box-sizing: border-box;
+}
+
+.flex-container .xboxone {
+    flex: 4 4 125px;
+    max-width: 250px;
+    max-height: auto;
+    box-sizing: border-box;
+}
+
+.flex-container .segams {
+    flex: 4 4 125px;
+    max-width: 250px;
+    max-height: auto;
+    box-sizing: border-box;
+}
+
+.flex-container .segagenesis {
+    flex: 4 4 125px;
+    max-width: 250px;
+    max-height: auto;
+    box-sizing: border-box;
+}
+
+.flex-container .snes {
+    flex: 8 8 250px;
+    max-width: 500px;
+    min-height: 125px;
+    max-height: auto;
+}
+
+.flex-container .n64 {
+    flex: 8 8 250px;
+    max-width: 500px;
+    min-height: 125px;
+    max-height: auto;
+}
+
+.flex-container .steam {
+    flex: 8 8 250px;
+    max-width: 500px;
+    min-height: 125px;
+    max-height: auto;
+}
+
+.flex-container .psx {
+    flex: 6 6 166.666666px;
+    max-width: 333.333333px;
+    max-height: auto;
+    box-sizing: border-box;
+}
+
+.flex-container .download {
+    flex: 6 6 166.666666px;
+    max-width: 333.333333px;
+    max-height: auto;
+    box-sizing: border-box;
+}
+
+.flex-container .gb {
+    flex: 6 6 166.666666px;
+    max-width: 333.333333px;
+    max-height: auto;
+    box-sizing: border-box;
+}
+
+.flex-container .gbc {
+    flex: 6 6 166.666666px;
+    max-width: 333.333333px;
+    max-height: auto;
+    box-sizing: border-box;
+}
+
+.flex-container .gba {
+    flex: 6 6 166.666666px;
+    max-width: 333.333333px;
+    max-height: auto;
+    box-sizing: border-box;
+}
+
+.flex-container .ds {
+    flex: 6 6 166.666666px;
+    max-width: 333.333333px;
+    max-height: auto;
+    box-sizing: border-box;
+}
+
+.flex-container .n3ds {
+    flex: 6 6 166.666666px;
+    max-width: 333.333333px;
+    max-height: auto;
+    box-sizing: border-box;
+}
+
+.flex-container .newn3ds {
+    flex: 6 6 166.666666px;
+    max-width: 333.333333px;
+    max-height: auto;
+    box-sizing: border-box;
+}
+
+.flex-container .segadreamcast {
+    flex: 6 6 166.666666px;
+    max-width: 333.333333px;
+    max-height: auto;
+    box-sizing: border-box;
+}
+
+.flex-container .ps3 {
+    flex: 5 5 142.857142857px;
+    max-width: 285.714285714px;
+    max-height: auto;
+    box-sizing: border-box;
+}
+
+.flex-container .ps4 {
+    flex: 5 5 142.857142857px;
+    max-width: 285.714285714px;
+    max-height: auto;
+    box-sizing: border-box;
+}
+
+.flex-container .psv {
+    flex: 5 5 142.857142857px;
+    max-width: 285.714285714px;
+    max-height: auto;
+    box-sizing: border-box;
+}
+
+.flex-container .switch {
+    flex: 5 5 133.333333333px;
+    max-width: 266.666666667px;
+    max-height: auto;
+    box-sizing: border-box;
+}
+
+.flex-container .psp {
+    flex: 5 5 133.333333333px;
+    max-width: 266.666666667px;
+    max-height: auto;
+    box-sizing: border-box;
+}
+
+.flex-container .segasaturn {
+    flex: 5 5 133.333333333px;
+    max-width: 266.666666667px;
+    max-height: auto;
+    box-sizing: border-box;
+}
+
+</style>
